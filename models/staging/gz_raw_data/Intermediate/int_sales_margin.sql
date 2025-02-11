7a40bff524ad6f766ef040d3c5ede4c076260573
kaufmannjosua1@gmail.com <kaufmannjosua1@gmail.com>
2025-02-11 16:14:47 +0000

    update margin

--- models/staging/gz_raw_data/Intermediate/int_sales_margin.sql
+++ models/staging/gz_raw_data/Intermediate/int_sales_margin.sql
@@ -1,6 +1,19 @@
-SELECT *,
-(quantity * purchase_price) AS purchase_cost,
-(revenue - purchase_price) AS margin
+-- SELECT *,
+-- (quantity * purchase_price) AS purchase_cost,
+-- (revenue - purchase_price) AS margin
+-- FROM {{ ref('stg_gz_raw_data__raw_gz_sales') }} AS sales
+--     LEFT JOIN {{ ref('stg_gz_raw_data__raw_gz_product') }} AS product
+--         on sales.products_id=product.products_id
+
+SELECT 
+    sales.orders_id,
+    sales.products_id AS sales_products_id,  -- Avoids column name conflict
+    sales.quantity,
+    sales.revenue,
+    product.products_id AS product_products_id,  -- Alias to avoid duplication
+    product.purchase_price,
+    COALESCE(sales.quantity, 0) * COALESCE(product.purchase_price, 0) AS purchase_cost,
+    COALESCE(sales.revenue, 0) - COALESCE(product.purchase_price, 0) AS margin
 FROM {{ ref('stg_gz_raw_data__raw_gz_sales') }} AS sales
-    LEFT JOIN {{ ref('stg_gz_raw_data__raw_gz_product') }} AS product
-        on sales.products_id=product.products_id
+LEFT JOIN {{ ref('stg_gz_raw_data__raw_gz_product') }} AS product
+    ON sales.products_id = product.products_id
