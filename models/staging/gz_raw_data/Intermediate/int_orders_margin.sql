44f2c231c30888cad7f83e13e69ff748f40da087
kaufmannjosua1@gmail.com <kaufmannjosua1@gmail.com>
2025-02-11 16:44:59 +0000

    with shipping data

--- models/staging/gz_raw_data/Intermediate/int_orders_margin.sql
+++ models/staging/gz_raw_data/Intermediate/int_orders_margin.sql
@@ -1,11 +1,5 @@
 SELECT 
-    sales.orders_id,
-    MAX(sales.date_date) AS date_date,  -- Assuming you need one date per order (e.g., latest)
-    SUM(sales.revenue) AS sum_revenue,
-    SUM(sales.quantity) AS sum_quantity,
-    SUM(COALESCE(sales.quantity, 0) * COALESCE(product.purchase_price, 0)) AS sum_purchase_cost,
-    SUM(COALESCE(sales.revenue, 0) - COALESCE(product.purchase_price, 0)) AS sum_margin
-FROM {{ ref('stg_gz_raw_data__raw_gz_sales') }} AS sales
-LEFT JOIN {{ ref('stg_gz_raw_data__raw_gz_product') }} AS product
-    ON sales.products_id = product.products_id
-GROUP BY sales.orders_id
+*
+FROM {{ ref('int_sales_margin') }} AS margin
+    LEFT JOIN {{ ref('stg_gz_raw_data__raw_gz_ship') }} AS ship
+    ON margin.orders_id = ship.orders_id
