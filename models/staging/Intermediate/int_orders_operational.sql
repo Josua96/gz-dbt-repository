46bfc49a543e827823ff656a93bb5acadd6d3985
kaufmannjosua1@gmail.com <kaufmannjosua1@gmail.com>
2025-02-12 11:27:36 +0000

    update with finance

--- models/staging/Intermediate/int_orders_operational.sql
+++ models/staging/Intermediate/int_orders_operational.sql
@@ -3,7 +3,9 @@ SELECT
     order_margin.date_date,
     (order_margin.margin + COALESCE(shipping.shipping_fee, 0) 
         - COALESCE(shipping.logcost, 0) 
-        - COALESCE(shipping.ship_cost, 0)) AS operational_margin
+        - COALESCE(shipping.ship_cost, 0)) AS operational_margin,
+    shipping.shipping_fee as shipping_fee,
+    shipping.logcost as logcost,
 FROM {{ ref('int_orders_margin') }} AS order_margin
 LEFT JOIN {{ ref('stg_gz_raw_data__raw_gz_ship') }} AS shipping
     ON order_margin.orders_id = shipping.orders_id
